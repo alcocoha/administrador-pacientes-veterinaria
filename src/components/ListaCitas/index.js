ce9fe755c7d1658b4eae754cb026c9a832081e7e
Jorge Hurtado <jorgehurtado@MacBook-Pro-de-MacBook.local>
2019-07-13 00:30:09 -0500

    mensaje cuando no hay citas

--- src/components/ListaCitas/index.js
+++ src/components/ListaCitas/index.js
@@ -3,11 +3,14 @@ import Cita from './../Cita';
 import PropTypes from 'prop-types';
 
 const ListaCitas = ({citas, eliminarCita}) => {
+
+    const mensaje = citas.length === 0 ? "No hay citas" : "Administrar las citas aquí";
+
     return (
         <div className="card mt-2 py-5">
             <div className="card-body">
                 <h2 className="card-title text-center">
-                    Administrar las citas aquí.
+                    { mensaje }
                 </h2>
                 <div className="lista-citas">
                     {
